9560f8de85cd9782fbf085a398f0c3c6eeeadbf0
comfortablecoffee <csdropout64@gmail.com>
2024-02-20 00:54:22 -0500

    added gifs to instameme

--- instameme/main.py
+++ instameme/main.py
@@ -2,8 +2,8 @@ import sys
 import random
 from PyQt5 import QtCore, QtWidgets, QtGui
 from PyQt5.QtWidgets import QMainWindow, QLabel, QGridLayout, QWidget, QPushButton
-from PyQt5.QtGui import QFont
-from PyQt5.QtCore import QSize
+from PyQt5.QtGui import QFont, QMovie
+from PyQt5.QtCore import QSize, Qt
 
 # from PyQt5.QtWidgets import *
 # from PyQt5.QtGui import *
@@ -17,13 +17,13 @@ class MainWindow(QMainWindow):
     '''
 
     #### Pre - Built && 
-    question = ">> >>> >>>> >>>" # by default
+    question = "Will You be my Valentine?" # by default
     title = "Playing Around"
-    positive_feedback = "OK NICE" # response if user picks button 1, default = yes
-    negative_feedback = "WRONG ANSWER" # response if user picks button 2, default = no
-    button_1 = "yes"
-    button_2 = "no"
-    button_2_alter = "sorry" #what button 2 will change to after user declines
+    positive_feedback = "Now what...?" # response if user picks button 1, default = yes
+    negative_feedback = ["WRONG ANSWER", "Please?", "Why so mean...?"] # response if user picks button 2, default = no
+    button_1 = "Yes"
+    button_2 = "No"
+    button_2_alter = "No (DONT)" #what button 2 will change to after user declines
 
     def create_label(self, name, movex, movey, rex, rey, fonttype, fontsize):
         l = QLabel(name, self)
@@ -50,14 +50,14 @@ class MainWindow(QMainWindow):
         self.setMinimumSize(QSize(1500, 900))    
         self.setWindowTitle(self.title) 
 
-        self.label = self.create_label(self.question, 300,200,900,200,'System',90)
+        self.label = self.create_label(self.question, 300,200,900,200,'System',75)
         self.label.setStyleSheet(
             """
             background-color: rgba(0, 0, 0, 0);
             color: white; 
             """
         )
-        self.pybutton = self.create_button(self.button_1, 300,500,200,100,'System',90,self.clickMethod)
+        self.pybutton = self.create_button(self.button_1, 300,500,200,100,'System',75,self.clickMethod)
         self.pybutton.setStyleSheet(
             """
             background-color: rgba(0, 0, 0, 0);
@@ -66,24 +66,49 @@ class MainWindow(QMainWindow):
         )
         self.pys = 200
         self.pys2 = 100
-        self.pybutton2 = self.create_button(self.button_2, 900,500,200,100,'System',90,self.clickMethod2)
+        self.pybutton2 = self.create_button(self.button_2, 900,500,200,100,'System',75,self.clickMethod2)
         self.pybutton2.setStyleSheet(
             """
             background-color: rgba(0, 0, 0, 0);
             color: white; 
             """
         )
+        
+        self.initUI()
+
+    def initUI(self):
+       
+        # Create a QLabel widget
+        self.gif = QLabel(self)
+        self.gif.setAlignment(Qt.AlignCenter)
+        self.set_gif('heart.gif')  # Set the path to your GIF file
+        self.gif.resize(0,0)
+
+    def set_gif(self, path):
+        # Create a QMovie object with the GIF file
+        movie = QMovie(path)
+
+        # Set the movie on the QLabel widget
+        self.gif.setMovie(movie)
+
+        # Start the movie
+        movie.start()
 
     def clickMethod(self):
         self.label.setText(self.positive_feedback)
         self.pybutton.resize(0,0)
         self.pybutton2.resize(0,0)
-        
+
+        self.gif.resize(200,200)
+        self.gif.move(400,400)
+
+
     def clickMethod2(self):
         if self.pys + 400 > 900:
             self.pybutton2.resize(0,0)
         self.pybutton2.move(random.randrange(0,1250),random.randrange(0,600))
-        self.label.setText(self.negative_feedback)
+        r = random.randint(0,len(self.negative_feedback)-1)
+        self.label.setText(self.negative_feedback[r])
         self.pybutton2.setText(self.button_2_alter)
         self.pybutton2.setFont(QFont('System', 32))
         self.pys += 20
@@ -95,4 +120,4 @@ if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     mainWin = MainWindow()
     mainWin.show()
-    sys.exit( app.exec_() )
+    sys.exit(app.exec_())
